14f5f261b1583a2e613e6d711099d9c8f42eff9e
Florian de Radiguès <florian.radigues@gmail.com>
2023-12-01 18:37:03 +0100

    day 1 refactored

--- challenges/day1.rb
+++ challenges/day1.rb
@@ -3,35 +3,31 @@ data = File.open(url).read
 
 def calibration(data)
     
-    numbersArray = data.split("\n")
-    .map { |line| line.gsub(/\D/,"") }
-    test = numbersArray.map do |number|
-        result = [number[0], number[-1]].join.to_i
+    answer = data.split("\n").map do |line| 
+        numbers = line.gsub(/\D/,"") 
+        [numbers[0], numbers[-1]].join.to_i
     end
-    test.sum
+    answer.sum
 
 end
 
 
 def calibration_2(data)
-    string_to_numbers = data.split("\n").map do |line|
-        line.gsub(/one/, "o1e")
-            .gsub(/two/, "t2o")
-            .gsub(/three/, "t3e")
-            .gsub(/four/, "f4r")
-            .gsub(/five/, "f5e")
-            .gsub(/six/, "s6x")
-            .gsub(/seven/, "s7n")
-            .gsub(/eight/, "e8t")
-            .gsub(/nine/, "e9e")
+    answer = data.split("\n").map do |line|
+        numbers = line.gsub(/one/, "o1e")
+                     .gsub(/two/, "t2o")
+                     .gsub(/three/, "t3e")
+                     .gsub(/four/, "f4r")
+                     .gsub(/five/, "f5e")
+                     .gsub(/six/, "s6x")
+                     .gsub(/seven/, "s7n")
+                     .gsub(/eight/, "e8t")
+                     .gsub(/nine/, "e9e")
+                     .gsub(/\D/,"")
+        [numbers[0], numbers[-1]].join.to_i
     end
 
-    numbersArray = string_to_numbers.map { |line| line.gsub(/\D/,"") }
-    test = numbersArray.map do |number|
-        result = []
-        result = [number[0], number[-1]].join.to_i
-    end
-    test.sum
+    answer.sum
 end
 
 
